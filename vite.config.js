6ddeaf156c9f1fff98b381d36e211ecf04016d7d
CharithaBimsara <charithabimsara@gmail.com>
2026-02-21 23:06:16 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: ['favicon.ico'],
+
+      manifest: {
+        name: 'DMS Sales App',
+        short_name: 'DMS',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#0d9488',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+
+  ],
+
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
   },
 })
